464fe6fff82011b0ab24e1f03fed171d3f33df46
Ross Baker <rossbaker32@gmail.com>
2020-07-07 10:10:45 +1000

    Solution to morning challenge

--- src/Autocomplete.js
+++ src/Autocomplete.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Autocomplete = ({possibleSearchResults}) => {
+
+    const [input, setInput] = useState('');
+    const [filteredSuggestions, setFilteredSuggestions] = useState([]);
+
+    const handleInput = async (e) => {
+        setInput(e.target.value);
+    }
+    
+    useEffect(() => {
+
+        if (input === "") {
+            setFilteredSuggestions([])
+        } else {
+            setFilteredSuggestions(possibleSearchResults.filter(
+                suggestion => {
+                    return suggestion.toLowerCase().includes(input)
+                }
+            ))
+        }
+    }, [input])
+
     return (
         <div>
-            Autocomplete Here
+            <input list="suggestions" value={input} onChange={e => handleInput(e)} type="text"></input>
+            <datalist id="suggestions">
+                {filteredSuggestions.slice(0, 5).map(suggestion => <option value={suggestion} />)}
+            </datalist>
         </div>
     )
 }
